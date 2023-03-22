4db8bfae311d05143791f8bd5dff4efa83625b47
Yissel Cuellar <105684108+Yissx@users.noreply.github.com>
2023-03-22 15:31:06 -0600

    Some organizational changes

--- HooksApp/src/HooksApp.jsx
+++ HooksApp/src/HooksApp.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react"
+import { CounterUseState } from "./counters/counterUseState"
+import { CounterCustomedHook } from "./counters/counterCustomedHook"
 
 export const HooksApp = () => {
-
-    const [counters, setCounters] = useState({
-        c1: 10,
-        c2: 20,
-        c3: 30
-    })
-    const {c1, c2, c3} = counters
-
-    return (
-    <>
+    
+    return(
+        <>
         <h1>Hello world from HooksApp</h1>
-        <hr></hr>
-        <h3>Counter 1: {c1}</h3>
-        <h3>Counter 2: {c2}</h3>
-        <h3>Counter 3: {c3}</h3>
-        <hr></hr>
+        <CounterUseState />
+        <CounterCustomedHook />
 
-       <button className="btn" onClick={ () => setCounters({ ...counters, c1: c1 + 1}) }>+1</button> 
-    </>)
+        </>
+    )
 }
---
+++ HooksApp/src/counters/counterUseState.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export const CounterUseState= () => {
+    const [counters, setCounters] = useState({
+        c1: 10,
+        c2: 20,
+        c3: 30
+    })
+    const {c1, c2, c3} = counters
+
+    return (
+    <>
+        
+        <hr></hr>
+        <h3>Counter that uses useState</h3>
+        <p>Counter 1: {c1}</p>
+        <p>Counter 2: {c2}</p>
+        <p>Counter 3: {c3}</p>
+        
+       <button className="btn" onClick={ () => setCounters({ ...counters, c1: c1 + 1}) }>+1</button> 
+    </>)
+}
